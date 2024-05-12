eb91f445b49fd9ab008fa48cd93388012896c30b
Forgot-Dream <56014859+Forgot-Dream@users.noreply.github.com>
2024-05-12 17:15:35 +0800

    bugfix: fix the dependence check in RecipeBookComponentMixin

--- src/main/java/dev/forgotdream/dma/mixins/RecipePatternAccessor.java
+++ src/main/java/dev/forgotdream/dma/mixins/RecipePatternAccessor.java
@@ -1,10 +1,14 @@
 package dev.forgotdream.dma.mixins;
 
+import dev.forgotdream.dma.Reference;
 import fi.dy.masa.itemscroller.recipes.RecipePattern;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import top.hendrixshen.magiclib.dependency.api.annotation.Dependencies;
+import top.hendrixshen.magiclib.dependency.api.annotation.Dependency;
 
+@Dependencies(and = @Dependency(Reference.ITEMSCROLLER_MOD_ID))
 @Mixin(value = RecipePattern.class,remap = false)
 public interface RecipePatternAccessor {
     @Accessor("result")
