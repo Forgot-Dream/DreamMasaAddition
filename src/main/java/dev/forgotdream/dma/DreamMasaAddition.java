b5adc4ae5bc990d55f6494d24cdd5dee141b19aa
Forgot-Dream <56014859+Forgot-Dream@users.noreply.github.com>
2024-05-14 16:19:41 +0800

    bugfix: remove ItemScroller version check. fixes: #1

--- src/main/java/dev/forgotdream/dma/DreamMasaAddition.java
+++ src/main/java/dev/forgotdream/dma/DreamMasaAddition.java
@@ -1,20 +1,20 @@
 package dev.forgotdream.dma;
 
 import dev.forgotdream.dma.config.Configs;
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
 import top.hendrixshen.magiclib.dependency.api.annotation.Dependencies;
 import top.hendrixshen.magiclib.dependency.api.annotation.Dependency;
 import top.hendrixshen.magiclib.malilib.impl.ConfigHandler;
 import top.hendrixshen.magiclib.malilib.impl.ConfigManager;
 
-public class DreamMasaAddition implements ModInitializer {
+public class DreamMasaAddition implements ClientModInitializer {
     private static final int CONFIG_VERSION = 1;
 
     @Dependencies(and={
-            @Dependency(value = "itemscroller",versionPredicate = ">=0.16.0", optional = true)
+            @Dependency(value = "itemscroller", optional = true)
     })
     @Override
-    public void onInitialize() {
+    public void onInitializeClient() {
         ConfigManager cm = ConfigManager.get(Reference.MOD_ID);
         cm.parseConfigClass(Configs.class);
         Reference.configHandler = new ConfigHandler(Reference.MOD_ID, cm, CONFIG_VERSION);
