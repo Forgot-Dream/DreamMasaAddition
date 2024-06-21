14b2bcd4a53d43933b0b3c73eaf55556c0c74b79
Forgot-Dream <56014859+Forgot-Dream@users.noreply.github.com>
2024-06-21 17:13:27 +0800

    chore: merge RecipeBookComponentMixin

--- src/main/java/dev/forgotdream/dma/mixins/RecipeBookComponentMixin.java
+++ src/main/java/dev/forgotdream/dma/mixins/RecipeBookComponentMixin.java
@@ -15,7 +15,6 @@ import net.minecraft.world.entity.player.StackedContents;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Recipe;
-import net.minecraft.world.item.crafting.RecipeHolder;
 import net.minecraft.world.item.crafting.ShapedRecipe;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -27,6 +26,10 @@ import top.hendrixshen.magiclib.dependency.api.annotation.Dependencies;
 import top.hendrixshen.magiclib.dependency.api.annotation.Dependency;
 
 import java.util.Objects;
+
+//#if MC>=12004
+import net.minecraft.world.item.crafting.RecipeHolder;
+//#endif
 
 @Dependencies(and = @Dependency(Reference.ITEMSCROLLER_MOD_ID))
 @Mixin(RecipeBookComponent.class)
@@ -41,16 +44,26 @@ public abstract class RecipeBookComponentMixin {
     @Shadow
     protected abstract void updateStackedContents();
 
+//#if MC<12004
+//$$ @Redirect(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;handlePlaceRecipe(ILnet/minecraft/world/item/crafting/Recipe;Z)V"))
+//$$    public void handlePlaceRecipe(MultiPlayerGameMode instance, int i, Recipe<?> recipes, boolean bl) {
+//$$        if (bl && Configs.quickCraftWithRecipeBook) {
+//$$            if (minecraft.level != null) {
+//$$                recipe(recipes);
+//$$            }
+//#else
     @Redirect(method = "mouseClicked", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/multiplayer/MultiPlayerGameMode;handlePlaceRecipe(ILnet/minecraft/world/item/crafting/RecipeHolder;Z)V"))
-    public void handlePlaceRecipe(MultiPlayerGameMode instance, int i, RecipeHolder recipeHolder, boolean bl) {
+    public void handlePlaceRecipe(MultiPlayerGameMode instance, int i, RecipeHolder recipes, boolean bl) {
         if (bl && Configs.quickCraftWithRecipeBook) {
             if (minecraft.level != null) {
-                recipe(recipeHolder.value());
+                recipe(recipes.value());
             }
+//#endif
         } else {
-            Objects.requireNonNull(this.minecraft.gameMode).handlePlaceRecipe(i, recipeHolder,bl);
+            Objects.requireNonNull(this.minecraft.gameMode).handlePlaceRecipe(i, recipes,bl);
         }
     }
+
 
     @Unique
     private void recipe(Recipe<?> recipe) {
