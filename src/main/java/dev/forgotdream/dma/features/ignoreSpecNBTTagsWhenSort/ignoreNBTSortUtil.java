0407453cd1452ab0351e11025ac17ad61a69fe8c
Forgot-Dream <56014859+Forgot-Dream@users.noreply.github.com>
2025-01-17 10:32:03 +0800

    bugfix: fix compat issue with itemscroller 0.24.55+ [Issue #7]

--- src/main/java/dev/forgotdream/dma/features/ignoreSpecNBTTagsWhenSort/ignoreNBTSortUtil.java
+++ src/main/java/dev/forgotdream/dma/features/ignoreSpecNBTTagsWhenSort/ignoreNBTSortUtil.java
@@ -1,25 +1,17 @@
 package dev.forgotdream.dma.features.ignoreSpecNBTTagsWhenSort;
 
 import dev.forgotdream.dma.config.Configs;
-import dev.forgotdream.dma.mixins.features.ignoreSpecNBTTagsWhenSort.InventoryUtilsAccessor;
-import fi.dy.masa.itemscroller.ItemScroller;
 import it.unimi.dsi.fastutil.Pair;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.item.ItemStack;
-
-import java.util.List;
 
 //#if MC<12006
 //$$ import net.minecraft.nbt.CompoundTag;
 //#else
 import net.minecraft.core.component.DataComponents;
-
-import static fi.dy.masa.itemscroller.util.InventoryUtils.swapSlots;
 //#endif
 
 public class ignoreNBTSortUtil {
-    private static Pair<Integer, Integer> lastSwapTry=Pair.of(-1, -1);
-
     public static boolean isIgnoredItem(ItemStack itemStack) {
         if (Configs.ignoreSpecNBTTagsWhenSort.getBooleanValue()) {
             for (var tag : Configs.ignoreSpecNBTTagsList.getStrings()) {
@@ -35,52 +27,4 @@ public class ignoreNBTSortUtil {
         }
         return false;
     }
-
-//#if MC>12006
-    /**
-     * quickSort() modified from fi.dy.masa.itemscroller.util.InventoryUtils#quickSort(AbstractContainerScreen, int, int)
-     */
-    public static void quickSort(AbstractContainerScreen<?> gui, int start, int end,boolean shulkerBoxFix) {
-        var itemList = gui.getMenu().getItems();
-        var ignoredList = itemList.subList(start, end).stream().filter(ignoreNBTSortUtil::isIgnoredItem).map(itemList::indexOf).toList();
-        lastSwapTry = Pair.of(-1, -1);
-        _quickSort(gui, start, end, ignoredList,shulkerBoxFix);
-    }
-
-    private static void _quickSort(AbstractContainerScreen<?> gui, int start, int end, List<Integer> ignoredList,boolean shulkerBoxFix) {
-        if (start < end) {
-            ItemStack mid = gui.getMenu().getSlot(end).getItem();
-            int l = start;
-            int r = end - 1;
-
-            while (l < r) {
-                while (l < r && (InventoryUtilsAccessor.invokeCompareStacks(gui.getMenu().getSlot(l).getItem(), mid,shulkerBoxFix) < 0 || ignoredList.contains(l))) {
-                    ++l;
-                }
-
-                while (l < r && (InventoryUtilsAccessor.invokeCompareStacks(gui.getMenu().getSlot(r).getItem(), mid,shulkerBoxFix) >= 0 || ignoredList.contains(r))) {
-                    --r;
-                }
-
-                if (l != r && (Integer)lastSwapTry.left() != l && (Integer)lastSwapTry.right() != r) {
-                    swapSlots(gui, l, r);
-                    lastSwapTry = Pair.of(l, r);
-                } else if (l != r) {
-                    ItemScroller.logger.warn("quickSort: Item swap failure. Duplicate pair of [{}, {}], cancelling sort task", l, r);
-                    return;
-                }
-            }
-
-            if (InventoryUtilsAccessor.invokeCompareStacks(gui.getMenu().getSlot(l).getItem(), gui.getMenu().getSlot(end).getItem(),shulkerBoxFix) >= 0
-                    && !(ignoredList.contains(l) || ignoredList.contains(end))) {
-                swapSlots(gui, l, end);
-            } else {
-                ++l;
-            }
-
-            _quickSort(gui, start, l - 1, ignoredList,shulkerBoxFix);
-            _quickSort(gui, l + 1, end, ignoredList,shulkerBoxFix);
-        }
-    }
-//#endif
 }
--- src/main/java/dev/forgotdream/dma/mixins/features/ignoreSpecNBTTagsWhenSort/InventoryUtilsMixin.java
+++ src/main/java/dev/forgotdream/dma/mixins/features/ignoreSpecNBTTagsWhenSort/InventoryUtilsMixin.java
@@ -1,10 +1,13 @@
 package dev.forgotdream.dma.mixins.features.ignoreSpecNBTTagsWhenSort;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import dev.forgotdream.dma.Reference;
 import dev.forgotdream.dma.config.Configs;
 import dev.forgotdream.dma.features.ignoreSpecNBTTagsWhenSort.ignoreNBTSortUtil;
 import fi.dy.masa.itemscroller.util.InventoryUtils;
+import it.unimi.dsi.fastutil.Pair;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Pseudo;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,6 +15,8 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import top.hendrixshen.magiclib.api.dependency.annotation.Dependencies;
 import top.hendrixshen.magiclib.api.dependency.annotation.Dependency;
+
+import java.util.ArrayList;
 
 @Dependencies(require = {
         @Dependency(Reference.ITEMSCROLLER_MOD_ID),
@@ -21,11 +26,10 @@ import top.hendrixshen.magiclib.api.dependency.annotation.Dependency;
 @Mixin(InventoryUtils.class)
 public abstract class InventoryUtilsMixin {
     //#if MC>12006
-    @Inject(method = "quickSort", at = @At(value = "HEAD"), remap = false, cancellable = true)
-    private static void redirectQuickSort$dma(AbstractContainerScreen<?> gui, int start, int end, boolean shulkerBoxFix, CallbackInfo ci) {
+    @Inject(method = "quickSort", at = @At(value = "INVOKE", target = "Ljava/util/ArrayList;size()I"), remap = false)
+    private static void afterFilter$dma(AbstractContainerScreen<?> gui, int start, int end, boolean shulkerBoxFix, int swapSlot, CallbackInfo ci, @Local ArrayList<Pair<Integer, ItemStack>> snapshot) {
         if (Configs.ignoreSpecNBTTagsWhenSort.getBooleanValue()) {
-            ignoreNBTSortUtil.quickSort(gui, start, end,shulkerBoxFix);
-            ci.cancel();
+            snapshot.removeIf((pair)->ignoreNBTSortUtil.isIgnoredItem(pair.value()));
         }
     }
     //#endif
